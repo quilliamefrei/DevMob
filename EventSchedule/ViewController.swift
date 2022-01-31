64adca83b70b39cab62e9a82ba35fcba269274d6
RemiRiandiere <remi.riandiere@gmail.com>
2022-01-31 16:00:59 +0100

    Passage d'un projet SwiftUI à un projet StoryBoard

---
+++ EventSchedule/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventSchedule
+//
+//  Created by Rémi Riandiere on 31/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
